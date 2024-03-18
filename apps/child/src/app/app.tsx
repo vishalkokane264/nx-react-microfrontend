3c539a76d721b22c0e4f2d0e33dc9c71ab032c56
vishalkokane264 <vishalkokane264@gmail.com>
2024-03-18 16:53:41 +0530

    file updated

--- apps/child/src/app/app.tsx
+++ apps/child/src/app/app.tsx
@@ -3,6 +3,8 @@
 import NxWelcome from './nx-welcome';
 
 export function App() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const abc = 'abc';
   return (
     <div>
       <div>hello santosh</div>
